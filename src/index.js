7945621949a1d9d11642be3deec84671c733e486
daeuneeee <dxexx2@gmail.com>
2023-07-01 04:43:16 +0900

    cors error 해결 -cors middleware 추가하여 해결결

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const postsRouter = require("./apis/posts");
 const { AppDataSource } = require("./data-source");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use(express.json());
 app.use("/posts", postsRouter);
 
 app.get("/", (req, res) => {
   res.send("hi! this is backend server for daniary");
-});
-
-app.listen(6060, () => {
-  console.log("hi! this is backend server for daniary");
 });
 
 AppDataSource.initialize()
@@ -21,3 +26,7 @@ AppDataSource.initialize()
   .catch((err) => {
     console.error(err);
   });
+
+app.listen(6060, () => {
+  console.log("hi! this is backend server for daniary");
+});
